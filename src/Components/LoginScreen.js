1b52171ffd0eed103e8c238254595eb7dd2332a9
Sparsh Bajaj <deadsoulriseagain@gmail.com>
2020-07-26 18:06:01 +0530

    Update LoginScreen.js

--- src/Components/LoginScreen.js
+++ src/Components/LoginScreen.js
@@ -53,9 +53,9 @@ const login = () => {
   const CLIENT_ID = '15bd6e805bd34f9fae0d28a2f2325964'; //process.env.REACT_APP_CLIENT_ID;
   let redirectUri;
   if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    redirectUri = 'http://localhost:3000/app';
-  } else {
     redirectUri = 'https://mudic.herokuapp.com/app';
+  } else {
+    redirectUri = 'https://mudic.herokuapp.com';
   }
   const scope = 'user-read-recently-played,user-top-read,playlist-modify-public';
   const authUri = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${redirectUri}&scope=${scope}`;
